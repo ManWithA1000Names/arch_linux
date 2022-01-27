c37ad5df85d0bdf4797d2e1e17ac63bd2dc434d1
Sir4.0 <loena.phivos2010@gmail.com>
2022-01-27 20:00:04 +0200

    feature(final script): added tide config

--- arch_linux_final.sh
+++ arch_linux_final.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd ~ || exit 1
-sudo aura -A "brave-bin" "rofi-git"
+sudo aura -A "brave-bin"
+sudo aura -A "rofi-git"
 gh auth login
-git clone https://github.com/ManWithA1000Names/dear-configs
+gh repo clone dear-configs
 cd dear-configs || exit 0
 ./deploy.fish
 cd ~ || exit
 sudo rm -rf ./arch_linux_final.sh
+fisher install ilancosman/tide
 lxappearance
-awesome-client "awesome.restart"
+awesome-client "awesome.restart()"
