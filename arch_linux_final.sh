9811c287040910d303eb8f0b556f0666c24eba15
Sir4.0 <loena.phivos2010@gmail.com>
2022-01-28 17:14:34 +0200

    feature(final script): set git protocol

--- arch_linux_final.sh
+++ arch_linux_final.sh
@@ -3,6 +3,7 @@ cd ~ || exit 1
 sudo aura --noconfirm -A "brave-bin"
 gh auth login
 gh repo clone dear-configs
+gh config set -h github.com git_protocol https
 cd dear-configs || exit 0
 ./deploy.fish
 timedatectl set-timezone "Europe/Athens"
