528d8dbd469cd0cbc463887d4d38357628a9a6eb
ManWithA1000Names <loena.phivos2010@gmail.com>
2022-02-04 21:11:19 +0200

    refactor(starting setup script): move lvim to final script

--- arch_linux_final.sh
+++ arch_linux_final.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-lxappearance &
 timedatectl set-timezone "Europe/Athens"
-sudo aura --noconfirm -A "brave-bin"
+lxappearance &>/dev/null &
+sudo aura --noconfirm -A "brave-bin" &>/dev/null &
+# formatters
+yarn global add prettier &>/dev/null &
+go install mvdan.cc/sh/v3/cmd/shfmt@latest &>/dev/null &
+pip install git+https://github.com/psf/black &>/dev/null &
+cargo install stylua &>/dev/null &
+# linters
+pip install flake8 codespell &>/dev/null &
+sudo aura --noconfirm -S shellcheck &>/dev/null &
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest &>/dev/null &
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+# julia language server
+mkdir -p ~/.julia/environments
+julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+mkdir -p ~/.config/lvim/ftplugin
+touch ~/.config/lvim/ftplugin/julia.lua
+{
+	echo "local opts = {}"
+	echo 'opts = require("lvim.lsp").get_common_opts()'
+	echo 'require("lspconfig").julials.setup(opts)'
+} >~/.config/lvim/ftplugin/julia.lua
+lvim ~/.config/lvim/config.lua
 wait
 cd ~ && sudo rm -rf ./arch_linux_final.sh
 sed -i '$ d' .bashrc
--- arch_linux_setup.sh
+++ arch_linux_setup.sh
@@ -177,31 +177,6 @@ sudo sed -i "s/Icon=.*/Icon=\/var\/lib\/AccountsService\/icons\/avatar.png/" "/v
 ###### end #######
 
 
-################
-### LunarVim ###
-################
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-# formatters
-yarn global add prettier
-go install mvdan.cc/sh/v3/cmd/shfmt@latest
-pip install git+https://github.com/psf/black
-cargo install stylua
-# linters
-pip install flake8 codespell
-sudo aura --noconfirm -S shellcheck
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-# julia language server
-mkdir -p ~/.julia/environments
-julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
-mkdir -p ~/.config/lvim/ftplugin
-touch ~/.config/lvim/ftplugin/julia.lua
-{
-	echo "local opts = {}"
-	echo 'opts = require("lvim.lsp").get_common_opts()'
-	echo 'require("lspconfig").julials.setup(opts)'
-} >~/.config/lvim/ftplugin/julia.lua
-###### end #####
-
 ###############
 ### CONFIGS ###
 ###############
@@ -209,6 +184,5 @@ git clone http://git.my.cloud/ManWithA1000Names/dear-configs.git
 cd dear-configs || exit 0
 ./deploy.fish
 mv ~/avatar.png "$HOME/.config/awesome/configuration/user-profile/$USER.png"
-##### end #####
-lvim ~/.config/lvim/config.lua
 echo "$HOME/arch_linux_final.sh" >>.bashrc
+##### end #####
