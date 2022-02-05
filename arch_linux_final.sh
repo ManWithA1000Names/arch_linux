e8bb038d1253cafc38a1db7388f91d5e5e832a3d
ManWithA1000Names <loena.phivos2010@gmail.com>
2022-02-05 18:38:51 +0200

    fix(final script): order of commands

--- arch_linux_final.sh
+++ arch_linux_final.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo aura --noconfirm -A "brave-bin" &>/dev/null &
 timedatectl set-timezone "Europe/Athens"
 lxappearance &>/dev/null &
+sudo aura --noconfirm -A "brave-bin" &>/dev/null &
 wait
 cd ~ && sudo rm -rf ./arch_linux_final.sh
