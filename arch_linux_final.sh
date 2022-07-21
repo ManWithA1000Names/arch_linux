8f84f1f8a3c5dfd59de95ce6de78b9eb941aa594
ManWithA1000Names <loena.phivos2010@gmail.com>
2022-07-21 14:56:57 +0300

    (final script):  upated sed paths

--- arch_linux_final.sh
+++ arch_linux_final.sh
@@ -5,8 +5,8 @@ lxappearance &>/dev/null &
 echo "gtk-decoration-layout=close,maximize,minimize:menu" >>~/.config/gtk-3.0/settings.ini
 rm -rf ~/.config/lvim/
 git clone https://github.com/manwitha1000names/lvim_config ~/.config/lvim
-sed -i 's/source("debug")/-- source("debug")/'
+sed -i 's/source("debug")/-- source("debug")/' ~/.config/lvim/config.lua
 lvim +PackerSync +q
-sed -i 's/-- source("debug")/source("debug")'
+sed -i 's/-- source("debug")/source("debug")' ~/.config/lvim/config.lua
 wait
 cd ~ && sudo rm -rf ./arch_linux_final.sh
