5df2d2e80761e88c1b3d3c04921d26a401b71578
ManWithA1000Names <loena.phivos2010@gmail.com>
2022-07-22 00:09:45 +0300

    fix(gimp): dont change the icon

--- install_photo_gimp.sh
+++ install_photo_gimp.sh
@@ -5,13 +5,9 @@ gimp_pid=$!
 curl https://codeload.github.com/Diolinux/PhotoGIMP/tar.gz/refs/tags/1.0 --output PhotoGIMP.tar.gz
 tar -xf ./PhotoGIMP.tar.gz
 rm ./PhotoGIMP.tar.gz
-mkdir -p ./.icons
-mkdir -p .local/share/applications/
 sleep 2
 kill $gimp_pid
 if cd PhotoGIMP-1.0; then
-	cp .icons/* ../.icons/
-	sudo sed -i 's/Icon=.*/Icon=photogimp.png/' /usr/share/applications/gimp.desktop
 	rm -rf ~/.config/GIMP/*
 	mv .var/app/org.gimp.GIMP/config/GIMP/2.10/ ../.config/GIMP/
 	cd ..
